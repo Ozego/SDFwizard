b7ee8b851b214d11ccf287341e7aa22a8c8911b3
Esben Jacob Sloth <esben@trueaxiongames.com>
2020-09-29 14:38:07 +0700

    refactor

---
+++ Assets/Packages/Ozeg/SDFWizard/Resources/Editor/SDFConverter.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Rendering;
+using Unity.Mathematics;
+
+namespace Ozeg.Tools
+{
+    public class SDFConverter
+    {
+        RenderTexture[] blitTextures = new RenderTexture[2];
+        Material material = null;
+        public SDFConverter()
+        {
+            material = new Material(Shader.Find("Hidden/JumpFlood"));
+        }
+
+        public Texture2D RenderSDF(Texture2D source, int distance, float treshold, int upsampling, System.Enum channel, System.Enum mode)
+        {
+            upsampling--;
+            int maxDim = math.max(source.width,source.height);
+            while(maxDim<<upsampling>2048) upsampling--;
+            upsampling = math.max(0,upsampling);
+            for (int i = 0; i < 2; i++) blitTextures[i] = SDFUtil.NewRenderTexture(source, upsampling);
+            int target = 0;
+            int jump = 2;
+            material.SetVector(
+                "_PixelParams",
+                new float4(
+                    blitTextures[target].width,
+                    blitTextures[target].height,
+                    math.length(new float2(blitTextures[target].width, blitTextures[target].height)),
+                    jump
+                )
+            );
+
+            if (source.wrapMode == TextureWrapMode.Repeat)
+                material.EnableKeyword( "__REPEAT");
+            else
+                material.DisableKeyword("__REPEAT");
+
+
+            var channelDict = new Dictionary<System.Enum,string>()
+            {
+                { SDFUtil.ColorChannel.Red,     "__RED" },
+                { SDFUtil.ColorChannel.Green,   "__GREEN" },
+                { SDFUtil.ColorChannel.Blue,    "__BLUE" },
+                { SDFUtil.ColorChannel.Alpha,    "__ALPHA" },
+                { SDFUtil.ColorChannel.mixRGB,  "__MIX" }
+            };
+            string channelKey;
+            if(channelDict.TryGetValue(channel, out channelKey)) material.EnableKeyword(channelKey);
+            material.SetFloat("_Treshold", treshold);
+            var exoTexture = new RenderTexture(blitTextures[target]);
+            Graphics.Blit(source,exoTexture,material,3);
+            Graphics.Blit(exoTexture, blitTextures[target], material, 0);
+            int size = math.min(source.width, source.height);
+            while (size > 1)
+            {
+                target^=1; jump<<=1; size>>=1;
+                material.SetVector(
+                    "_PixelParams",
+                    new float4(
+                        blitTextures[target].width,
+                        blitTextures[target].height,
+                        math.length(new float2(blitTextures[target].width, blitTextures[target].height)),
+                        jump
+                    )
+                );
+                Graphics.Blit(blitTextures[target^1], blitTextures[target], material, 1);
+            }
+            target^=1;
+            Graphics.Blit(exoTexture,blitTextures[target], material, 4);
+            Graphics.Blit(blitTextures[target^1],exoTexture);
+            size = math.min(source.width, source.height);
+            jump = 2;
+            target^=1;
+            Graphics.Blit(blitTextures[target^1], blitTextures[target], material, 0);
+            while (size > 1)
+            {
+                target^=1; jump<<=1; size>>=1;
+                material.SetVector(
+                    "_PixelParams",
+                    new float4(
+                        blitTextures[target].width,
+                        blitTextures[target].height,
+                        math.length(new float2(blitTextures[target].width, blitTextures[target].height)),
+                        jump
+                    )
+                );
+                Graphics.Blit(blitTextures[target^1], blitTextures[target], material, 1);
+            }
+
+            target^=1;
+            var outTexture = new RenderTexture(
+                source.width, 
+                source.height, 
+                8, RenderTextureFormat.ARGB32
+            );
+    
+            var modeDict = new Dictionary<System.Enum,string>()
+            {
+                { SDFUtil.RenderingMode.ContourUV,      "__MODE_UV_CONTOUR" },
+                { SDFUtil.RenderingMode.ContourRGB,     "__MODE_RGB_CONTOUR" },
+                { SDFUtil.RenderingMode.DistanceOnly,   "__MODE_DIST" },
+                { SDFUtil.RenderingMode.NearestUV,      "__MODE_UV" },
+                { SDFUtil.RenderingMode.RGBDistance,    "__MODE_RGB_DIST" },
+                { SDFUtil.RenderingMode.RGBOnly,        "__MODE_RGB" },
+            };
+            string modeKey;
+            if(modeDict.TryGetValue(mode, out modeKey)) material.EnableKeyword(modeKey);
+
+            material.SetFloat("_Distance", distance<<upsampling);
+            material.SetTexture("_ExoTex", exoTexture);
+            material.SetTexture("_EndoTex", blitTextures[target^1]);
+            Graphics.Blit(source,outTexture, material, 2);
+            // Debug.Log($"ST:{source.width} RT:{blitTextures[target].width}, {channelKey}, {modeKey}");
+            return SDFUtil.RenderTextureToTexture2D(outTexture);
+        }
+    }
+
+    public static class SDFUtil
+    {
+        public enum RenderingMode
+        {
+            DistanceOnly,
+            RGBOnly,
+            RGBDistance,
+            NearestUV,
+            ContourUV,
+            ContourRGB
+        }
+        
+        public enum ColorChannel
+        {
+            Red,
+            Green,
+            Blue,
+            Alpha,
+            mixRGB
+        }
+
+        public static RenderTexture NewRenderTexture(Texture2D source)
+        {
+            return NewRenderTexture(source, 1);
+        }
+        public static RenderTexture NewRenderTexture(Texture2D source, int upsampling)
+        {
+
+            var rt = new RenderTexture
+            (
+                source.width<<upsampling,
+                source.height<<upsampling,
+                16,
+                RenderTextureFormat.ARGBHalf
+            );
+            rt.antiAliasing = 2;
+            rt.anisoLevel = 0;
+            rt.filterMode = FilterMode.Bilinear;
+            rt.useMipMap = false;
+            rt.wrapMode = source.wrapMode;
+            return rt;
+        }
+
+        public static Texture2D RenderTextureToTexture2D(this RenderTexture rt)
+        {
+            var tex = new Texture2D(rt.width, rt.height, TextureFormat.RGBA32, false);
+            RenderTexture.active = rt;
+            tex.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+            tex.Apply();
+            return tex;
+        }
+    }
+}
---
+++ Assets/Packages/Ozeg/SDFWizard/Resources/Editor/SDFWizard.cs
@@ -0,0 +1,157 @@
+using UnityEditor;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+namespace Ozeg.Tools
+{
+    public class SDFWizard : EditorWindow
+    {
+        [MenuItem("Tools/SDFWizard")]
+        public static void ShowExample()
+        {
+            SDFWizard wnd = GetWindow<SDFWizard>();
+            wnd.minSize = new Vector2(288,340);
+            wnd.titleContent = new GUIContent("SDFWizard");
+        }
+
+        public void OnEnable()
+        {
+            VisualElement root = rootVisualElement;
+            VisualElement vt = Resources.Load<VisualTreeAsset>("Editor/SDFWizard").Instantiate();
+            vt.styleSheets.Add(Resources.Load<StyleSheet>("Editor/SDFWizard"));
+            root.Add(vt);
+            Image           dropBox         = root.Q<Image>         ("DropBox");
+            SliderInt       sizeSlider      = root.Q<SliderInt>     ("SizeSlider");
+            IntegerField    sizeField       = root.Q<IntegerField>  ("SizeField");
+            Slider          tresholdSlider  = root.Q<Slider>        ("TresholdSlider");
+            FloatField      tresholdField     = root.Q<FloatField>    ("TresholdField");
+            SliderInt       sampleSlider    = root.Q<SliderInt>     ("SampleSlider");
+            IntegerField    sampleField     = root.Q<IntegerField>  ("SampleField");
+            EnumField       channelSelect   = root.Q<EnumField>     ("ChannelSelect");
+            Box             channelDisplay  = root.Q<Box>           ("ChannelDisplay");
+            EnumField       modeSelect = root.Q<EnumField>     ("RenderingSelect");
+            channelSelect.Init(SDFUtil.ColorChannel.Alpha);
+            modeSelect.Init(SDFUtil.RenderingMode.DistanceOnly);
+            bool validated = false;
+
+            dropBox.RegisterCallback<DragEnterEvent>((e)=>
+            {
+                foreach (var item in DragAndDrop.objectReferences)
+                {
+                    if(item.GetType() == typeof(Texture2D)) validated = true;
+                }
+                dropBox.tintColor = validated ? new Color(.6f, .94f, .2f) : new Color(.94f, .3f, .2f);
+            });
+
+            dropBox.RegisterCallback<DragUpdatedEvent>((e)=>
+            {
+                if(validated) DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+            });
+
+            dropBox.RegisterCallback<DragPerformEvent>((e)=>
+            {
+                if(validated) for (int i = 0; i < DragAndDrop.objectReferences.Length; i++)
+                {
+                    var item = (UnityEngine.Object)DragAndDrop.objectReferences[i];
+                    string path = DragAndDrop.paths[i];
+                    if(item.GetType() == typeof(Texture2D))
+                    {
+                        // string newPath = path.Insert(path.LastIndexOf("."),"_SDF");
+                        string newPath = path.Substring(0,path.LastIndexOf("."))+"_SDF.png";
+                        string outPath = Application.dataPath.Substring(0,Application.dataPath.Length-6)+newPath;
+                        var texture = item as Texture2D;
+                        var converter = new SDFConverter();
+                        var outData = converter.RenderSDF(texture, sizeField.value, tresholdField.value, sampleField.value, channelSelect.value, modeSelect.value);
+                        System.IO.File.WriteAllBytes(outPath,outData.EncodeToPNG());
+                        AssetDatabase.Refresh();
+                        var importer = (TextureImporter)AssetImporter.GetAtPath(newPath);
+                        var importerSettings = new TextureImporterSettings();
+                        ((TextureImporter)AssetImporter.GetAtPath(path)).ReadTextureSettings(importerSettings);
+                        importer.SetTextureSettings(importerSettings);
+                        importer.sRGBTexture &= modeSelect.value.Equals(SDFUtil.RenderingMode.RGBDistance);
+                        importer.textureCompression = TextureImporterCompression.Uncompressed;
+                        importer.SaveAndReimport();
+                        AssetDatabase.ImportAsset(newPath);
+                    }
+                }
+            });
+
+            dropBox.RegisterCallback<DragExitedEvent>((e)=>
+            {
+                validated=false;
+                dropBox.tintColor=Color.white;
+            });
+
+            dropBox.RegisterCallback<DragLeaveEvent>((e)=>
+            {
+                validated=false;
+                dropBox.tintColor=Color.white;
+            });
+
+            sizeSlider.RegisterCallback<ChangeEvent<int>>((e)=>
+            {
+                sizeField.value=2<<e.newValue;
+            });
+
+            sizeField.RegisterCallback<ChangeEvent<int>>((e)=>
+            {
+                int c = 0;
+                int v = e.newValue;
+                while(v>2) { c++; v>>=1; }
+                sizeSlider.value=c;
+                sizeField.value=2<<c;
+            });
+            
+            tresholdSlider.RegisterCallback<ChangeEvent<float>>((e)=>
+            {
+                tresholdField.value=e.newValue;
+            });
+
+            tresholdField.RegisterCallback<ChangeEvent<float>>((e)=>
+            {
+                tresholdSlider.value=e.newValue;
+            });
+            sampleSlider.RegisterCallback<ChangeEvent<int>>((e)=>
+            {
+                sampleField.value=e.newValue;
+            });
+
+            sampleField.RegisterCallback<ChangeEvent<int>>((e)=>
+            {
+                sampleSlider.value=e.newValue;
+            });
+
+            channelSelect.RegisterCallback<ChangeEvent<Enum>>((e)=>
+            {
+                switch (e.newValue)
+                {
+                    case SDFUtil.ColorChannel.Red: 
+                        channelDisplay.style.backgroundColor = new Color(.94f, .3f, .2f); 
+                        break;
+                    case SDFUtil.ColorChannel.Green: 
+                        channelDisplay.style.backgroundColor = new Color(.6f, .94f, .2f); 
+                        break;
+                    case SDFUtil.ColorChannel.Blue: 
+                        channelDisplay.style.backgroundColor = new Color(.2f, .6f, .94f); 
+                        break;
+                    case SDFUtil.ColorChannel.Alpha: 
+                        channelDisplay.style.backgroundColor = Color.grey; 
+                        break;
+                    default: 
+                        channelDisplay.style.backgroundColor = Color.white;
+                        break;
+                }
+            });
+        }
+
+        public void OnDisable()
+        {
+        }
+        
+    }
+
+}
